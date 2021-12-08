85861d17c337f67c0084e37c8cff9f31f45c266c
andrie.balbino <andrie.anderson1@gmail.com>
2021-12-08 19:54:31 -0300

    Adicionado paginação na listagem de tópicos

--- src/main/java/com/api/learningSpringBoot/LearningSpringBootApplication.java
+++ src/main/java/com/api/learningSpringBoot/LearningSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.api.learningSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class LearningSpringBootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/api/learningSpringBoot/Repository/TopicoRepository.java
+++ src/main/java/com/api/learningSpringBoot/Repository/TopicoRepository.java
@@ -1,7 +1,7 @@
 package com.api.learningSpringBoot.Repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.learningSpringBoot.model.Topico;
@@ -9,6 +9,6 @@ import com.api.learningSpringBoot.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 }
