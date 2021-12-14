8edeec80aeeef5060ff76ea498c0456b6d13d0cf
andrie.balbino <andrie.anderson1@gmail.com>
2021-12-14 19:35:01 -0300

    adicionado lógica de autenticação

--- src/main/java/com/api/learningSpringBoot/model/Usuario.java
+++ src/main/java/com/api/learningSpringBoot/model/Usuario.java
@@ -1,21 +1,32 @@
 package com.api.learningSpringBoot.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 @Entity
 @Table(name = "usuario", schema = "cursospringboot")
-public class Usuario {
+public class Usuario implements UserDetails{
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private Collection<Perfil> perfis = new ArrayList<Perfil>();
 
 	@Override
 	public int hashCode() {
@@ -72,6 +83,41 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis ;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
